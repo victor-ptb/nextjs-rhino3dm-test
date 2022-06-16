5a87c82255a6d17bb6aa5a8457963c8afef992df
victor-martinez-ptblink <victor.martinez@ptblink.com>
2022-06-16 22:57:17 +1000

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { dev }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
